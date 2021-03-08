dcfbd2b964d1c1d4ccb93a8aa6a4338775e5f8f3
Bank <60050230@kmitl.ac.th>
2021-03-08 15:32:45 +0700

    Create calculator class

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'calculator.dart';
+import 'calculator_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Calculator(),
+    home: CalculatorPage(),
     debugShowCheckedModeBanner: false,
   ));
 }
